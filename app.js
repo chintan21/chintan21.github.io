16aaf15cf2049b0e313b78beeb059e1bcc37dd80
Chintan Dabhi <cdabhi37@gmail.com>
2019-03-01 13:47:06 +0530

    Create app.js
    Script added

---
+++ app.js
@@ -0,0 +1,8 @@
+var database = firebase.database();
+function writeUserData(userId, name, email, imageUrl) {
+  firebase.database().ref('users/' + userId).set({
+    username: name,
+    email: email,
+    profile_picture : imageUrl
+  });
+}
